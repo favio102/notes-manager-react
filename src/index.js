352d071cbc919db96f32fb34910a9f5f206ba919
Favio Valero <favio102@protonmail.com>
2024-03-19 06:59:12 -0700

    redux configuration added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import "./index.css";
+import ReactDOM from "react-dom/client";
+import React from "react";
+import { App } from "./App";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import { StrictMode } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
 );
