fb5803cdd63b13e629fff55d1781e396cfa95f15
Favio Valero <favio102@protonmail.com>
2024-03-24 03:27:36 -0700

    app with internal database, function ok, signin and signup page adde but not functional. App ready to link to Firebase

--- src/components/ButtonPrimary/ButtonPrimary.jsx
+++ src/components/ButtonPrimary/ButtonPrimary.jsx
@@ -1,9 +1,9 @@
-export function ButtonPrimary({ children, onClick, isDisabled }) {
+export function ButtonPrimary({ type, children, onClick, isDisabled }) {
   return (
     <button
       disabled={isDisabled}
       onClick={onClick}
-      type="button"
+      type={type}
       className="btn btn-danger rounded-pill fw-bold btn-lg py-2 px-xl-5"
     >
       {children}
