56d5d50de01940cbb31bcebc9a20ae66ba479ffa
Favio Valero <favio102@protonmail.com>
2024-03-23 20:56:45 -0700

    basic style added to signin page

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -1,5 +1,23 @@
-import s from "./style.module.css"
+import { ButtonPrimary } from "components/ButtonPrimary/ButtonPrimary";
+import s from "./style.module.css";
+import { Link } from "react-router-dom";
 
 export function Signin(props) {
-  return <>Signin</>
+  const form = (
+    <div className={s.formContainer}>
+      <h2 className={s.title}>
+        Signin <br /> to access your team notes.
+      </h2>
+      <form action="" className={s.formGroup}>
+        <input type="text" />
+        <input type="text" />
+        <ButtonPrimary className={s.btn}>Sign in</ButtonPrimary>
+        <span>
+          Don't have an account yet? <Link to='/signup'>Signup</Link>
+        </span>
+      </form>
+    </div>
+  );
+
+  return <>{form}</>;
 }
