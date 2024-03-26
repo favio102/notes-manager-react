a5f49e89fdab0a9cf80eadf20c8586cd5074ec4d
Favio Valero <favio102@protonmail.com>
2024-03-26 05:48:57 -0700

    persisting the redux auth slice

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./index.css";
 import ReactDOM from "react-dom/client";
 import React from "react";
 import { Provider } from "react-redux";
-import { store } from "./store";
+import { persistor, store } from "./store";
 import { StrictMode } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { NoteBrowse } from "pages/NoteBrowse/NoteBrowse";
@@ -13,6 +13,7 @@ import { ProtectedApp } from "App";
 import { Signin } from "pages/Signin/Signin";
 import { Signup } from "pages/Signup/Signup";
 import { FirebaseApp } from "utils/firebase";
+import { PersistGate } from "redux-persist/integration/react";
 
 FirebaseApp.init();
 
@@ -21,18 +22,20 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/" element={<ProtectedApp />}>
-            <Route path="/" element={<NoteBrowse />} />
-            <Route path="/note/:noteId" element={<Note />} />
-            <Route path="/note/new" element={<NoteCreate />} />
-            <Route path="*" element={<PageNotFound />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/" element={<ProtectedApp />}>
+              <Route path="/" element={<NoteBrowse />} />
+              <Route path="/note/:noteId" element={<Note />} />
+              <Route path="/note/new" element={<NoteCreate />} />
+              <Route path="*" element={<PageNotFound />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
