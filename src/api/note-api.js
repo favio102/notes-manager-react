5e2231f3e6ffca6c91f6871c169fb39a5b166bb8
Favio V <favio102@protonmail.com>
2024-03-27 11:56:18 +0900

    fetching a collection from firestore

--- src/api/note-api.js
+++ src/api/note-api.js
@@ -1,21 +1,31 @@
-import axios from "axios";
+// const BASE_URL = "http://localhost:3200/notes";
 
-const BASE_URL = "http://localhost:3200/notes";
+import { FirebaseApp } from "utils/firebase";
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
+
 
 export class NoteAPI {
   static async create(formValues) {
-    return (await axios.post(`${BASE_URL}`, formValues)).data;
+    // return (await axios.post(`${BASE_URL}`, formValues)).data;
   }
   static async fetchAll() {
-    return (await axios.get(`${BASE_URL}`)).data;
+    // return (await axios.get(`${BASE_URL}`)).data;
+    const q = query(collection(FirebaseApp.db, 'notes'), orderBy('created_at', 'asc'));
+    const response = await getDocs(q)
+    return response.docs.map((document) => {
+      return {
+        id: document.id,
+        ... document.data()
+      }
+    })
   }
   static async deleteById(noteId) {
-    return (await axios.delete(`${BASE_URL}/${noteId}`)).data;
+    // return (await axios.delete(`${BASE_URL}/${noteId}`)).data;
   }
   static async updateById(id, values) {
-    return (await axios.patch(`${BASE_URL}/${id}`, values)).data;
+    // return (await axios.patch(`${BASE_URL}/${id}`, values)).data;
   }
-  static async fetchById(noteId) {
-    return (await axios.get(`${BASE_URL}/${noteId}`)).data;
-  }
+  // static async fetchById(noteId) {
+  //   return (await axios.get(`${BASE_URL}/${noteId}`)).data;
+  // }
 }
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,13 +1,16 @@
 import { firebaseConfig } from "config";
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 export class FirebaseApp {
   static firebaseApp = undefined;
   static auth = undefined;
+  static db = undefined;
   static init() {
     this.firebaseApp = initializeApp(firebaseConfig);
     this.auth = getAuth();
     signInWithEmailAndPassword(this.auth, "test0@gmail.com", "test0test0");
+    this.db = getFirestore(this.firebaseApp);
   }
 }
